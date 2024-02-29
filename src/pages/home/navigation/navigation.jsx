950db8dd80c6d9488e43dbf8a21976cd9bbd1f80
romanesss <romagavrilyuk2015@gmail.com>
2024-02-29 22:48:14 +0200

    testing update[2]

--- src/pages/home/navigation/navigation.jsx
+++ src/pages/home/navigation/navigation.jsx
@@ -24,7 +24,7 @@ class Navigation extends Component {
             <div className='navigation container text'>
                 <nav className='nav__list'>
                     <ul>
-                        <a className="nav__text text" href='#'><li>Woman’s XXXXXXXXXXXXXXX</li></a>
+                        <a className="nav__text text" href='#'><li>Woman’s XXXXXXXXXXXXXXXX</li></a>
                         <a className="nav__text text" href='#'><li>Men’s Fashion</li></a>
                         <a className="nav__text text" href='#'><li>Electronics</li></a>
                         <a className="nav__text text" href='#'><li>Home & Lifestyle</li></a>
